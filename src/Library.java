bf512b8c1d680826a202831eee1a1199c3d3b315
Dong Deng <ddong@thoughtworks.com>
2012-03-13 15:26:43 +0800

    Initial commit

---
+++ src/Library.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: twer
+ * Date: 3/13/12
+ * Time: 3:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Library {
+}
